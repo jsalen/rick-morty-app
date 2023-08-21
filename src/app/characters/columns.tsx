548a40496efd8eccc524318702a5edba36b5fa55
Joseph Salen <jsalen.91@gmail.com>
2023-08-21 10:36:56 -0400

    feat: add Edit table row into Characters and Episodes page

--- src/app/characters/columns.tsx
+++ src/app/characters/columns.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { RowActions } from '@/components/DataTable/RowActions';
+import { CharacterForm } from '@/components/DataTable/CharacterForm';
+import { DeleteCharacter } from '@/components/DataTable/DeleteCharacter';
 import { type Character, type Location } from '@/lib/types';
 import { type ColumnDef } from '@tanstack/react-table';
 
@@ -48,15 +49,25 @@ export const columns: Array<ColumnDef<CharacterHeader>> = [
     accessorKey: 'location',
     header: 'Location',
     cell: ({ row }) => {
-      const { name } = row.original.origin;
+      const { name } = row.original.location;
 
       return name;
     },
   },
   {
     id: 'actions',
+    header: 'Actions',
     cell: ({ row }) => {
-      return <RowActions payload={row.original as Character} />;
+      return (
+        <div className="flex gap-4">
+          <CharacterForm
+            formAction="update"
+            character={row.original as Character}
+            triggerHeading="Edit"
+          />
+          <DeleteCharacter id={row.original.id} />
+        </div>
+      );
     },
   },
 ];
